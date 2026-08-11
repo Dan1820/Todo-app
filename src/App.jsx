ee5f569c2ad2873f2d0f6324515f158025d51ec0
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2026-08-11 18:33:19 +0300

    filtering and editing tasks

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,18 @@ import { nanoid } from "nanoid"
 import FilterButton from "./Components/FilterButton"
 import Todo from "./Components/Todo"
 
+const FILTER_MAP={
+  All:()=>true,
+  Active:(task) =>!task.completed,
+  Completed: (task) =>task.completed,
+}
+
+const FILTER_NAMES= Object.keys(FILTER_MAP)
+
 
 const App=(props)=>{
   const [tasks,setTasks]=useState(props.tasks)
+  const[filter,setFilter]= useState("All")
 
   const addTask=(name)=>{
     const newTask={id:`todo-${nanoid()}`, name,completed:false};
@@ -25,13 +34,24 @@ const App=(props)=>{
     setTasks(updatedTasks)
   }
 
+  const filterList= FILTER_NAMES.map((name)=>(
+    <FilterButton 
+    key={name} 
+    name={name} 
+    isPressed={name===filter}
+    setFilter={setFilter} />
+
+  ))
+
   const deleteTask=(id)=>{
     const remainingTasks= tasks.filter((task) => id !==task.id)
     setTasks(remainingTasks)
   }
 
 
-  const taskList= tasks?.map((task)=>(
+  const taskList= tasks
+  .filter(FILTER_MAP[filter])
+  .map((task)=>(
 <Todo 
 id={task.id} 
 name={task.name} 
@@ -39,7 +59,33 @@ completed={task.completed}
 key={task.id}
 toggleTaskCompleted={toggleTaskCompleted}
 deleteTask={deleteTask}
+editTask={editTask}
 />))
+function editTask(id, newName) {
+  const editedTaskList = tasks.map((task) => {
+    // if this task has the same ID as the edited task
+    if (id === task.id) {
+      // Copy the task and update its name
+      return { ...task, name: newName };
+    }
+    // Return the original task if it's not the edited task
+    return task;
+  });
+  setTasks(editedTaskList);
+}
+// const editTask=(id,newName)=>{
+//   const editedTaskList=tasks.map((task)=>{
+//     //if this task has the same id as edited task,
+//     //coppy task and update its name
+//     if(id=== task.id){
+//       return{...task, name:newName}
+//     }else{
+//       return task //return the original name 
+//     }
+//   })
+//   setTasks(editedTaskList)
+// }
+
 const tasksNoun= taskList.length !==1 ? 'tasks': 'task'
 const headingText=`${taskList.length} ${tasksNoun} remaining`
 
@@ -49,9 +95,7 @@ const headingText=`${taskList.length} ${tasksNoun} remaining`
       <Form addTask={addTask} />
       
         <div className="filters btn-group stack-exception" >
-          <FilterButton />
-          <FilterButton />
-          <FilterButton />
+          {filterList}
         </div>
         
         
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -31,6 +31,7 @@ const Form=(props)=> {
         autoComplete="off"
         value={name}
         onChange={handleChange}
+        
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
